862831e20359f8183f510e13dafdc96040d6376c
OorjitVedamurthy123 <63858193+OorjitVedamurthy123@users.noreply.github.com>
2020-05-16 00:48:06 +0530

    Added one more character

---
+++ Hyena.js
@@ -0,0 +1,18 @@
+class Hyena extends BaseClass {
+    constructor(x,y){
+      
+      super(x,y,90,90);
+      this.image = loadImage("hyena.png");
+      
+      
+    }
+  
+    display() {
+     
+      super.display();
+    
+  
+      
+    }
+  }
+  
---
+++ Lion.js
@@ -0,0 +1,17 @@
+class Lion extends BaseClass {
+  constructor(x,y){
+    
+    super(x,y,90,90);
+    this.image = loadImage("lion.png");
+    
+    
+  }
+
+  display() {
+   
+    super.display();
+  
+
+    
+  }
+}
